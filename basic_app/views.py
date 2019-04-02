42fc197a1044b94e2ca2c24b997f2ad46b5691df
larryvsdata <larryvsdata@gmail.com>
2019-04-02 11:23:02 -0700

    Templates added

--- basic_app/views.py
+++ basic_app/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request , 'basic_app/index.html')
+
+def other(request):
+    return render(request , 'basic_app/other.html')
+
+def relative(request):
+    return render(request , 'basic_app/relative_url_templates.html')
