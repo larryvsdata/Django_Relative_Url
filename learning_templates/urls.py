d9f3d56d773f8d49e246bf07d160209aff570259
larryvsdata <larryvsdata@gmail.com>
2019-04-09 10:49:48 -0700

    Finished

--- learning_templates/urls.py
+++ learning_templates/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from basic_app import views
+from django.conf.urls import include
+
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^admin/', admin.site.urls),
